b10f413bae12952557179c01a291048ff3c63393
Larysa Bielova <afi.pallada@gmail.com>
2021-01-10 00:09:23 +0200

    static HTML, CSS, adaptive CSS

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/cities', usersRouter);
-
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
 
